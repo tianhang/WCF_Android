7dfe48c58039980bac4b8d280d8df2e7e3acf76a
tianhang <476855396@qq.com>
2015-09-07 10:36:35 +0800

    add my service

---
+++ Service/MyService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Service
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "MyService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select MyService.svc or MyService.svc.cs at the Solution Explorer and start debugging.
+    public class MyService : IMyService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
